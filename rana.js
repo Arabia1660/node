0431685e78b6c638c40e02b7e3cd7a83cb822585
Arabia1660 <arabia1660@gmail.com>
2025-07-14 17:49:32 +0600

    adding rana.js file

---
+++ rana.js
@@ -0,0 +1,14 @@
+const getName =() => {
+    return " Arabia hossain";
+}
+const getAge =() => {
+    return '34';
+}
+
+// exports.getName = getName; 
+// exports.getAge = getAge; 
+module.exports = {
+    getAge,
+    getName
+
+}
